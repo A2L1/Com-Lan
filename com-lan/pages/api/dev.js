28eeb3cf48981ee108614bd4cc9b325e42d7ab5c
A2L1 <pro.arossignol@gmail.com>
2022-04-06 10:54:03 +0200

    Add SQLite database

--- com-lan/pages/api/dev.js
+++ com-lan/pages/api/dev.js
@@ -1,6 +1,7 @@
 import net from "net";
 import os from "os";
 import network from "network";
+import { PrismaClient } from "@prisma/client";
 
 const getPrivateIp = async () =>
   new Promise((resolve, reject) =>
@@ -13,12 +14,30 @@ const getPrivateIp = async () =>
     })
   );
 
+const getToBDD = async (obj) => {
+  const prisma = new PrismaClient();
+  const device = await prisma.device
+    .create({
+      data: {
+        username: "A2L2",
+        address: "127.0.0.2",
+      },
+    })
+    .catch((e) => {
+      throw e;
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+};
+
 export default async (req, res) => {
   const { method } = req;
   switch (method) {
     case "GET": {
       try {
         const ipa = await getPrivateIp();
+        getToBDD();
         res.status(200).send({ text: "requete GET", interface: ipa });
       } catch (error) {
         res.status(400).json({});
