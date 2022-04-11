1dc626bca389dc3d9d4d2dac339bf9a1c6a9a5fc
A2L1 <pro.arossignol@gmail.com>
2022-04-11 10:34:11 +0200

    Calcul broadcast address of the subnet

--- com-lan/pages/api/dev.js
+++ com-lan/pages/api/dev.js
@@ -1,8 +1,18 @@
-import net from "net";
-import os from "os";
 import network from "network";
 import { PrismaClient } from "@prisma/client";
 
+const IpToBinString = (ip) => {
+  //Crée une liste des octets de l'adresse entrée
+  let arrayNetmask = ip.split(".");
+  //Convertit chaque octet en valeur binaire
+  for (let i = 0; i <= arrayNetmask.length - 1; i++) {
+    const number = arrayNetmask[i];
+    const bin = parseInt(number).toString(2).padStart(8, "0");
+    arrayNetmask[i] = bin;
+  }
+
+  return arrayNetmask.join("");
+};
 const getPrivateIp = async () =>
   new Promise((resolve, reject) =>
     network.get_private_ip((err, ip) => {
@@ -14,6 +24,37 @@ const getPrivateIp = async () =>
     })
   );
 
+const getBroadcast = async (ip) =>
+  new Promise((resolve, reject) =>
+    network.get_active_interface((err, interfaceObj) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      //Récupération du masque réseau en une seule string des valeurs binaires
+      const stringMask = IpToBinString(interfaceObj["netmask"]);
+      //Récupère l'indice du premier zero dans la string binaire du masque
+      const firstZero = stringMask.indexOf("0");
+      //Récupère l'ip de l'interface en une seule string des valeurs binaires
+      const ipStringBin = IpToBinString(ip);
+      //Garde la partie réseau de l'ip de l'interface
+      const baseSlice = ipStringBin.slice(0, firstZero);
+      //Ajoute tous les bits à 1 pour créer l'addresse de broadcast en fonction du masque
+      const broadcastBin = baseSlice + "1".repeat(32 - firstZero);
+      //Créer une liste de string où chaque élément contient une valeur en binaire (un octet par élément)
+      let arrayBroadcast = broadcastBin.match(/.{1,8}/g);
+      let broadcast = [];
+      //Transforme chaque élément (binaire) en valeur décimale
+      arrayBroadcast.forEach((elem) => {
+        broadcast.push(parseInt(elem, 2));
+      });
+      //Renvoie l'adresse de broadcast sous forme de string et le masque
+      resolve({
+        broadcast: broadcast.join("."),
+        mask: interfaceObj["netmask"],
+      });
+    })
+  );
 const getToBDD = async (obj) => {
   const prisma = new PrismaClient();
   const device = await prisma.device
@@ -37,8 +78,13 @@ export default async (req, res) => {
     case "GET": {
       try {
         const ipa = await getPrivateIp();
-        getToBDD();
-        res.status(200).send({ text: "requete GET", interface: ipa });
+        const broadcast = await getBroadcast(ipa);
+        //getToBDD();
+        res.status(200).send({
+          text: "requete GET",
+          interface: ipa,
+          broadcast: broadcast,
+        });
       } catch (error) {
         res.status(400).json({});
       }
--- com-lan/pages/components/index/mod_index.js
+++ com-lan/pages/components/index/mod_index.js
@@ -1,6 +1,5 @@
 import { Formik, Field } from "formik";
 //import * as yup from "yup";
-//import FormField from "./components/FormField";
 
 const initialValues = { username: "" };
 
@@ -27,38 +26,4 @@ const Form = () => {
     </Formik>
   );
 };
-// prisme OREM backend sqlite
 export default Form;
-
-/*import { Formik, Field } from "formik";
-
-const initialValues = { username: "" };
-const Form = () => {
-  const handleFormSubmit = async (values) => {
-    console.log(values);
-  };
-
-  return (
-    <Formik initialValues={initialValues} onSubmit={handleFormSubmit}>
-      {({ handleSubmit, isSubmitting, isValid }) => (
-        <form onSubmit={handleSubmit} noValidate>
-          <p>
-            <Field
-              as="input"
-              name="username"
-              type="username"
-              placeholder="Username"
-            />
-          </p>
-          <p>
-            <button type="submit">Submit</button>
-          </p>
-        </form>
-      )}
-      ;
-    </Formik>
-  );
-};
-
-export default Form;
-*/
